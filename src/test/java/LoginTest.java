2ab3fcc839a1674c7a911e22cb3d4c1cc6619fe4
Girinivas <girinivasnm@gmail.com>
2019-09-05 21:29:41 +0530

    My second commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+		
+		driver = new FirefoxDriver();
+		
+	}
+	
+	@Test
+	public void doLogin(){
+		
+		driver.get("https://www.amazon.in/");
+	}
+	
+	
+	
+	
+}
